8175ab545f0f31ce567c4ad27b6b1c1e38af686d
nam <namish@tuta.io>
2026-07-23 15:29:35 +0530

    fix stupid blur

--- src/main.zig
+++ src/main.zig
@@ -2,25 +2,37 @@ const std = @import("std");
 const OpenGraph = @import("opengraph.zig").OpenGraph;
 
 pub fn main() !void {
-    // const img = try OpenGraph
-    //     .init()
-    //     .bg_image("biscuit.png")
-    //     .blur(10)
-    //     .overlay("#000000aa")
-    //     .title("writing my own static site because i love fixing fixed stuff", "#ffffffff")
-    //     .subtitle("i wrote this in zig", "#ffffffff")
-    //     .bottom("/blog", "#ffffffff")
-    //     .pfp("static/images/pfp.png")
-    //     .save("out.png");
-    // defer img.deinit();
+    const img = try OpenGraph
+        .init()
+        .bg_image("biscuit.png")
+        .blur(10)
+        .overlay("#000000aa")
+        .title("writing my own static site generator because i love fixing stuff that already works", "#ffffffff")
+        .subtitle("i wrote this in zig (btw)", "#ffffffff")
+        .bottom("/blog", "#ffffffff")
+        .pfp("static/images/pfp.png")
+        .save("out.png");
+    defer img.deinit();
 
-    // const simple = try OpenGraph
-    //     .init()
-    //     .bg_linear_gradient("#0f0f0fff", "#222222ff", .LeftToRight)
-    //     .grid(40.0, 0.5, "#323232ff")
-    //     .title("this should be the new blog post that i will never release ", "#ffffffff")
-    //     .subtitle("but well i will never release it", "#ffffffff")
-    //     .pfp("static/images/pfp.png")
-    //     .save("simple.png");
-    // defer simple.deinit();
+    const simple = try OpenGraph
+        .init()
+        .bg_linear_gradient("#0f0f0fff", "#222222ff", .LeftToRight)
+        .grid(40.0, 0.5, "#323232ff")
+        .title("showcasing a simple linear gradient background", "#ffffffff")
+        .subtitle("some text below it", "#ffffffff")
+        .pfp("static/images/temp.png")
+        .save("simple.png");
+    defer simple.deinit();
+
+    const lg = try OpenGraph
+        .init()
+        .bg_linear_gradient("#77b6edff", "#8be8a7ff", .LeftToRight)
+        .save("simple2.png");
+    defer lg.deinit();
+
+    const x = try OpenGraph.init()
+        .bg_image("bg2.png")
+        .overlay("#000000aa")
+        .save("outter.png");
+    defer x.deinit();
 }
--- src/opengraph.zig
+++ src/opengraph.zig
@@ -176,60 +176,49 @@ pub const OpenGraph = struct {
 
     pub fn blur(self: *const Self, radius: u8) *const Self {
         std.debug.assert(radius <= MaxRadius);
-
         c.cairo_surface_flush(self.surface);
         const stride: usize = @intCast(c.cairo_image_surface_get_stride(self.surface));
         const ptr = c.cairo_image_surface_get_data(self.surface);
         const pixels: [*]u32 = @ptrCast(@alignCast(ptr));
-
         const window = @as(u32, radius) * 2 + 1;
 
-        var pass: u8 = 0;
-
-        while (pass < 3) : (pass += 1) {
-            for (0..HEIGHT) |y| {
-                const row: [*]const u32 =
-                    @ptrCast(@alignCast(@as([*]const u8, @ptrCast(pixels)) + y * stride));
-
-                for (0..WIDTH) |x| {
-                    var a: u32 = 0;
-                    var r: u32 = 0;
-                    var g: u32 = 0;
-                    var b: u32 = 0;
-
-                    var i: i32 = -@as(i32, radius);
-                    while (i <= radius) : (i += 1) {
-                        const xx: usize = @intCast(std.math.clamp(
-                            @as(i32, @intCast(x)) + i,
-                            0,
-                            WIDTH - 1,
-                        ));
-
-                        const p = row[xx];
-                        a += (p >> 24) & 0xff;
-                        r += (p >> 16) & 0xff;
-                        g += (p >> 8) & 0xff;
-                        b += p & 0xff;
-                    }
-
-                    blur_buffer[y * WIDTH + x] =
-                        ((a / window) << 24) |
-                        ((r / window) << 16) |
-                        ((g / window) << 8) |
-                        (b / window);
-                }
-            }
-        }
         for (0..HEIGHT) |y| {
-            const row: [*]u32 =
-                @ptrCast(@alignCast(@as([*]u8, @ptrCast(pixels)) + y * stride));
-
+            const row: [*]const u32 =
+                @ptrCast(@alignCast(@as([*]const u8, @ptrCast(pixels)) + y * stride));
             for (0..WIDTH) |x| {
                 var a: u32 = 0;
                 var r: u32 = 0;
                 var g: u32 = 0;
                 var b: u32 = 0;
+                var i: i32 = -@as(i32, radius);
+                while (i <= radius) : (i += 1) {
+                    const xx: usize = @intCast(std.math.clamp(
+                        @as(i32, @intCast(x)) + i,
+                        0,
+                        WIDTH - 1,
+                    ));
+                    const p = row[xx];
+                    a += (p >> 24) & 0xff;
+                    r += (p >> 16) & 0xff;
+                    g += (p >> 8) & 0xff;
+                    b += p & 0xff;
+                }
+                blur_buffer[y * WIDTH + x] =
+                    ((a / window) << 24) |
+                    ((r / window) << 16) |
+                    ((g / window) << 8) |
+                    (b / window);
+            }
+        }
 
+        for (0..HEIGHT) |y| {
+            const row: [*]u32 =
+                @ptrCast(@alignCast(@as([*]u8, @ptrCast(pixels)) + y * stride));
+            for (0..WIDTH) |x| {
+                var a: u32 = 0;
+                var r: u32 = 0;
+                var g: u32 = 0;
+                var b: u32 = 0;
                 var i: i32 = -@as(i32, radius);
                 while (i <= radius) : (i += 1) {
                     const yy: usize = @intCast(std.math.clamp(
@@ -237,14 +226,12 @@ pub const OpenGraph = struct {
                         0,
                         HEIGHT - 1,
                     ));
-
                     const p = blur_buffer[yy * WIDTH + x];
                     a += (p >> 24) & 0xff;
                     r += (p >> 16) & 0xff;
                     g += (p >> 8) & 0xff;
                     b += p & 0xff;
                 }
-
                 row[x] =
                     ((a / window) << 24) |
                     ((r / window) << 16) |
